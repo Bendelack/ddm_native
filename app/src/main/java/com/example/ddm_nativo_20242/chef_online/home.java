0856518124cfd9ab7c17d3259be3737aa0c48ca6
Bendelack <rickk1010@gmail.com>
2025-05-09 11:50:47 -0300

    inicio telas

---
+++ app/src/main/java/com/example/ddm_nativo_20242/chef_online/home.java
@@ -0,0 +1,16 @@
+package com.example.ddm_nativo_20242.chef_online;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ddm_nativo_20242.R;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/example/ddm_nativo_20242/chef_online/login_chef.java
@@ -0,0 +1,16 @@
+package com.example.ddm_nativo_20242.chef_online;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ddm_nativo_20242.R;
+
+public class login_chef extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_chef);
+    }
+}
